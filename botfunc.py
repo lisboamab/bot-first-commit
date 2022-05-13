205487325a65886afd64dc6e2c4053c066d1e043
lisboamab <lisboamab@gmail.com>
2022-05-13 15:38:33 -0300

    script para abrir programas

---
+++ botfunc.py
@@ -0,0 +1,24 @@
+def abrirPrograma(programa: str) -> None:    
+    from pyautogui import click, write, press, PAUSE
+
+    PAUSE = 2
+
+    # abrir o programa
+    press("win")
+    write(programa)
+    press("enter")
+
+    return None
+
+def siteDesejado(site: str) -> None:
+    from pyautogui import click, write, press
+
+teste = 1
+
+while True:
+    teste = input("Digite o nome do programa que quer abrir: ")
+    if int(teste) != 0:
+        abrirPrograma(teste)
+    else:
+        print("Programa encerrando")
+        break
